38ebe98a77ca3b87bf5c4b8422f052eaf7b2ee46
Valentin <valentin.rudloff.perso@gmail.com>
2022-08-04 22:25:23 +0200

    update env

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -6,10 +6,14 @@ import { urlFor, client } from '../../client'
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
-  
+
   useEffect(() => {
-    const query = '*[_type == "abouts"]'
-    client.fetch(query).then(data => setAbouts(data))}, []);
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
@@ -24,7 +28,7 @@ const About = () => {
           transition={{duration: 0.5, type: 'tween'}}
           className='app__profile-item'
           key={about.title+index}>
-          <img src={urlFor(about.urlImg)} alt={about.title}/>
+          <img src={urlFor(about.imgUrl)} alt={about.title}/>
           <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
           <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
         </motion.div>
