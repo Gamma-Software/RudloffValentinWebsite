3bd6f6fd1d357873dc3f526f6bf7141f8fef16e2
Valentin <valentin.rudloff.perso@gmail.com>
2022-08-05 23:02:57 +0200

    update work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {userState, useEffect} from 'react';
+import { AiFillEye, AiFillGithub} from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
-import './Work.scss'
+import {AppWrap} from '../../wrapper';
+import { urlFor, client } from '../../client';
+
+import './Work.scss';
 
 const Work = () => {
   return (
-    <div>Work</div>
-  )
+    <>
+      <h2 className='head-text'>
+        My creative <span>Porfolio </span>
+        <div>
+          <ul>test</ul>
+        </div>
+      </h2>
+    </>
+  )  
 }
 
-export default Work
+export default AppWrap(Work, 'work')
