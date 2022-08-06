bdf0e781329bdc1367c9fc282897406b276a0173
Valentin <valentin.rudloff.perso@gmail.com>
2022-08-06 12:26:51 +0200

    update work

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,4 +1,4 @@
-import React, {userState, useEffect} from 'react';
+import React, {userState, useEffect, useState} from 'react';
 import { AiFillEye, AiFillGithub} from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
@@ -8,16 +8,51 @@ import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1});
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+    client.fetch(query).then(data => {
+      setWorks(data);
+      setFilterWork(data);
+  }, [])})
+  
+  const handleWorkFilter = (item) => {
+  }
+
   return (
     <>
       <h2 className='head-text'>
         My creative <span>Porfolio </span>
-        <div>
-          <ul>test</ul>
-        </div>
+      <div className='app__work-filter'>
+        {['UI', 'Web App', 'Mob App', 'Photography', 'Aerial Photography', 'Drone', 'Science'].map((item, index) => (
+          <div 
+            key={index} 
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}>
+              {item}
+          </div>
+        ))}
+      </div>
       </h2>
+      <motion.div
+        animate={animateCard}
+        transition={{duration: 0.5, delayChildren: 0.5}}
+        className='app__work-portfolio'
+        >
+          {filterWork.map((work, index) => (
+            <div key={index} className='app__work-item app__flex'>
+              <div className='app__work-img app__flex'>
+                <img src={urlFor(work.imgUrl)} alt={work.name} />
+              </div>
+            </div>
+          ))}
+      </motion.div>
     </>
-  )  
+  )
 }
 
 export default AppWrap(Work, 'work')
