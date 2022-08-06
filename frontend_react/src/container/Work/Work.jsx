921dbe0998417ea6edc7c919ff7e9a535e1ee97a
Valentin <valentin.rudloff.perso@gmail.com>
2022-08-06 23:32:03 +0200

    finish works

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,4 +1,4 @@
-import React, {userState, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { AiFillEye, AiFillGithub} from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
@@ -8,27 +8,41 @@ import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
-  const [activeFilter, setActiveFilter] = useState('All');
-  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1});
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
     const query = '*[_type == "works"]';
-    client.fetch(query).then(data => {
+
+    client.fetch(query).then((data) => {
       setWorks(data);
       setFilterWork(data);
-  }, [])})
-  
+    });
+  }, []);
+
   const handleWorkFilter = (item) => {
-  }
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
 
   return (
     <>
       <h2 className='head-text'>
         My creative <span>Porfolio </span>
       <div className='app__work-filter'>
-        {['UI', 'Web App', 'Mob App', 'Photography', 'Aerial Photography', 'Drone', 'Science'].map((item, index) => (
+        {['Embedded App', 'Web App', 'Mobile App', 'Photography', 'Aerial Photography', 'All'].map((item, index) => (
           <div 
             key={index} 
             onClick={() => handleWorkFilter(item)}
@@ -47,6 +61,38 @@ const Work = () => {
             <div key={index} className='app__work-item app__flex'>
               <div className='app__work-img app__flex'>
                 <img src={urlFor(work.imgUrl)} alt={work.name} />
+                <motion.div
+                  whileHover={{opacity: [0, 1]}}
+                  transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+                  className='app__work-hover app__flex'>
+                  <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                    whileInView={{scale: [0, 1]}}
+                    whileHover={{scale: [1, 0.9]}}
+                    transition={{duration: 0.25}}
+                    className='app__flex'>
+                      <AiFillEye/>
+                    </motion.div>
+                  </a>
+                  <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                    whileInView={{scale: [0, 1]}}
+                    whileHover={{scale: [1, 0.9]}}
+                    transition={{duration: 0.25}}
+                    className='app__flex'>
+                      <AiFillGithub/>
+                    </motion.div>
+                  </a>
+                </motion.div>
+              </div>
+              <div
+                className='app__work-content app__flex'
+              >
+                <h4 className='bold-text'>{work.title}</h4>
+                <p className='p-text' style={{marginTop: 10}}>{work.description}</p>
+                <div className='app__work-tag app_flex'>
+                  <p className='p-text'>{work.tags[0]}</p>
+                </div>
               </div>
             </div>
           ))}
